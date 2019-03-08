be9f35e900dc418f79d0a7b76a47419e22e5d112
minute <243560619@qq.com>
2019-03-08 19:00:31 +0800

    tsconfig

--- redux-playgroud/src/index.ts
+++ redux-playgroud/src/index.ts
@@ -1,39 +1,54 @@
-// import { createStore, Reducer, applyMiddleware, Middleware } from "redux";
+import { Reducer, Middleware, createStore, applyMiddleware } from "redux";
+// import {
+//   createStore,
+//   combineReducers,
+//   bindActionCreators,
+//   applyMiddleware,
+//   compose
+// } from "./reduxSource";
 // import logger from "redux-logger";
 
 // // interface todoState {
 // //   menuList:
 // // }
-// const initialState = {
-//   menuList: [],
-//   expandMenus: [],
-//   activeMenu: undefined
-// };
+const initialState: any = {
+  menuList: [],
+  expandMenus: [],
+  activeMenu: undefined
+};
 
 // const commonMiddleWare: Middleware = ({ getState, dispatch }) => {
 //   return () => dispatch;
 // };
 
-// const todoApp: Reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "UPDATE_MENU":
-//       return Object.assign({}, state, {
-//         menuList: action.data
-//       });
-//   }
-// };
+const todoApp: Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_MENU":
+      return Object.assign({}, state, {
+        menuList: action.data
+      });
+  }
+};
 
-// const store = createStore(todoApp);
+const commonMiddleWare: Middleware = ({ getState, dispatch }) => next => {
+  console.log("2", next);
+  return action => {
+    console.log("1", dispatch);
+    next(action);
+  };
+};
 
-// store.dispatch({
-//   type: "UPDATE_MENU",
-//   data: [{ key: 1 }]
-// });
+const commonMiddleWare2: Middleware = ({ getState, dispatch }) => next => {
+  console.log("4", next);
+  return action => {
+    console.log("5", dispatch);
+    next(action);
+  };
+};
 
-import {
-  createStore,
-  combineReducers,
-  bindActionCreators,
-  applyMiddleware,
-  compose
-} from "./reduxSource";
+const store = createStore(todoApp, applyMiddleware(commonMiddleWare, commonMiddleWare2));
+
+store.dispatch({
+  type: "UPDATE_MENU",
+  data: [{ key: 1 }]
+});
